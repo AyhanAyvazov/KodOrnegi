ea41afed04ecaa23065d264b54a2f233bf7bed5a
AyhanAyvazov <ayhanayvazov00@gmail.com>
2022-07-29 00:39:29 +0400

    kod

---
+++ MyFinalProject/Core/Utilities/Results/Result.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+     
+        public Result(bool success, string message) : this(success)
+        {
+            Message = message;
+        }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public Result(bool success)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+            Success = success;
+        }
+
+
+        public bool Success { get;}
+
+        public string Message { get;}
+    }
+}
